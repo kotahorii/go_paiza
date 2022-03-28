b5ce5e9ae265f28509ea9f5efe703c66f7607de7
kotahorii <khorii.micron@gmail.com>
2022-03-28 18:04:03 +0900

    solve question

--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-)
-
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	var x, y int
-	if sc.Scan() {
-		if num, err := strconv.Atoi(sc.Text()); err != nil {
-			log.Fatal("input invalid value")
-		} else {
-			x = num
-		}
-	}
 
-	if sc.Scan() {
-		if num, err := strconv.Atoi(sc.Text()); err != nil {
-			log.Fatal("input invalid value")
-		} else {
-			y = num
-		}
-	}
-
-	PrintAddAndSub(x, y)
-}
-
-func PrintAddAndSub(x, y int) {
-	fmt.Printf("sum is %d\n", x+y)
-	fmt.Printf("sub is %d", x-y)
 }
--- question/variable.go
+++ question/variable.go
@@ -11,3 +11,12 @@ func DivideNumber(num int) {
 		fmt.Printf("remainder %d, quotient %d\n", num%i, num/i)
 	}
 }
+
+func PrintAddAndSub(x, y int) {
+	fmt.Printf("sum is %d\n", x+y)
+	fmt.Printf("sub is %d", x-y)
+}
+
+func PrintMeanValue(x, y int) {
+	fmt.Println((x + y) / 2)
+}
