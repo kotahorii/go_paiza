9a690420e4b1a59456e296002189cbfbe6cb33b6
kotahorii <khorii.micron@gmail.com>
2022-04-04 15:12:42 +0900

    solve problem

--- main.go
+++ main.go
@@ -3,6 +3,18 @@ package main
 import "go/paiza/question"
 
 func main() {
-	cat := question.NewCat("takashi", 43)
-	question.PrintAnimalName(cat)
+	cat1 := question.NewCat("cat1", 43)
+	cat2 := question.NewCat("cat2", 32)
+	dog1 := question.NewDog2("dog1", 33)
+	dog2 := question.NewDog2("dog2", 32)
+
+	animalSlice := []interface {
+		ShowProfile()
+		Speak()
+	}{
+		cat1,
+		dog1,
+		cat2,
+		dog2,
+	}
 }
--- question/class2.go
+++ question/class2.go
@@ -4,6 +4,15 @@ import "fmt"
 
 type Animal interface {
 	ShowProfile()
+	Speak()
+}
+
+func PrintAnimalName(a Animal) {
+	a.ShowProfile()
+}
+
+func AnimalSpeak(a Animal) {
+	a.Speak()
 }
 
 type Cat struct {
@@ -19,10 +28,31 @@ func (cat *Cat) ShowProfile() {
 	fmt.Printf("%s's age is %d\n", cat.name, cat.age)
 }
 
+func (cat *Cat) Speak() {
+	fmt.Println("nya~")
+}
+
 func (cat *Cat) Sleep() {
 	fmt.Println("zzz...")
 }
 
-func PrintAnimalName(a Animal) {
-	a.ShowProfile()
+type Dog2 struct {
+	name string
+	age  int
+}
+
+func NewDog2(name string, age int) *Dog2 {
+	return &Dog2{name, age}
+}
+
+func (dog *Dog2) ShowProfile() {
+	fmt.Printf("%s's age is %d\n", dog.name, dog.age)
+}
+
+func (dog *Dog2) Speak() {
+	fmt.Println("bow")
+}
+
+func (dog *Dog2) Run() {
+	fmt.Println("Go straight")
 }
