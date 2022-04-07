adf1116a16ba17ce20e2aca511ead127c5a5e0b1
kotahorii <khorii.micron@gmail.com>
2022-04-07 17:13:54 +0900

    solve question

--- python_rdy/organize_list.go
+++ python_rdy/organize_list.go
@@ -1,1 +1,15 @@
 package pythonrdy
+
+func OrganizeList(unOrganizedList [][]string) map[string][]string {
+	// if not define result here, nil map error occurred
+	result := map[string][]string{}
+	for _, l := range unOrganizedList {
+		_, ok := result[l[0]]
+		if ok {
+			result[l[0]] = append(result[l[0]], l[1])
+		} else {
+			result[l[0]] = []string{l[1]}
+		}
+	}
+	return result
+}
--- python_rdy/organize_list_test.go
+++ python_rdy/organize_list_test.go
@@ -1,1 +1,24 @@
 package pythonrdy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestOrganizeList(t *testing.T) {
+	l := [][]string{
+		{"Py", "PyA"},
+		{"Py", "PyB"},
+		{"Py", "PyC"},
+		{"Pe", "PeA"},
+		{"Pe", "PeB"},
+		{"Pe", "PeC"},
+	}
+	actual := OrganizeList(l)
+	expected := map[string][]string{
+		"Py": {"PyA", "PyB", "PyC"},
+		"Pe": {"PeA", "PeB", "PeC"},
+	}
+	require.Equal(t, expected, actual)
+}
