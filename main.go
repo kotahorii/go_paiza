8c96e642a35f4a43656fb5c03403e527afc9311a
kotahorii <khorii.micron@gmail.com>
2022-04-09 21:06:07 +0900

    solve question

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 )
 
 func main() {
-	// fmt.Println(binary_search([]int{1, 2, 3, 4, 5}, 1))
-	// fmt.Println(insertionSort([]int{5, 3, 1, 6, 2}))
 	fmt.Println(mergeSort([]int{5, 3, 1, 6, 2}))
 }
 
@@ -15,6 +13,7 @@ func binary_search(a_list []int, n int) bool {
 
 	for last >= first {
 		mid := (first + last) / 2
+
 		if n == a_list[mid] {
 			return true
 		}
@@ -31,7 +30,6 @@ func binary_search(a_list []int, n int) bool {
 func insertionSort(a_list []int) []int {
 	for i := 1; i < len(a_list); i++ {
 		value := a_list[i]
-		fmt.Println(value)
 		for i > 0 && a_list[i-1] > value {
 			a_list[i] = a_list[i-1]
 			i -= 1
@@ -45,13 +43,15 @@ func mergeSort(a_list []int) []int {
 	if len(a_list) > 1 {
 		copied := make([]int, len(a_list))
 		copy(copied, a_list)
-		mid := len(a_list) / 2
 
+		mid := len(a_list) / 2
 		left_half, right_half := copied[:mid], copied[mid:]
+
 		mergeSort(left_half)
 		mergeSort(right_half)
 
 		left_ind, right_ind, list_ind := 0, 0, 0
+
 		for left_ind < len(left_half) && right_ind < len(right_half) {
 			if left_half[left_ind] >= right_half[right_ind] {
 				a_list[list_ind] = right_half[right_ind]
